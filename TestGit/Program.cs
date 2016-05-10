152a8942ae64d1b850531dfb782449377e2d39f3
mcleuren <mariella.cleuren@vdab.be>
2016-05-10 09:35:26 +0200

    Hallo

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,7 +10,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Goede morgen");
+            Console.WriteLine("Hallo");
         }
     }
 }
