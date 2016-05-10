dd6dfd2184cceeba0b521d31ad735f7cc55cb493
vdab <f@ff>
2016-05-10 10:45:28 +0200

    aanpassing + extra lijn

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -11,6 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Welkom door Ronny2");
+            Console.WriteLine("Welkom Mariella");
         }
     }
 }
