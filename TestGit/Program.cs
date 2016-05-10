eb40bcef273bd6bde1ebce3d52c0b003960cc40b
mcleuren <mariella.cleuren@vdab.be>
2016-05-10 10:44:21 +0200

    Welkom door Mariella

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,7 +10,7 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welkom door Ronny2");
+            Console.WriteLine("Welkom door Mariella");
         }
     }
 }
